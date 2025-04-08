cf909223692bdb79c5a515aa0dd33a4131cc71dd
ThalesDinizio <thales.dionizio@gmail.com>
2025-04-08 19:51:51 -0300

    ADD Imagens

--- index/script.js
+++ index/script.js
@@ -1,8 +1,8 @@
 document.getElementById('playButton').addEventListener('click', () => {
-    window.location.href = '/EcoSnake/projetoa3.html';
+    window.location.href = 'EcoSnake/Game.html';
   });
   
   document.getElementById('aboutButton').addEventListener('click', () => {
-    window.location.href = 'sobre.html';
+    window.location.href = 'SobreProjeto/SobreProjeto.html';
   });
   
