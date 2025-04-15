e557150989ea4f5bd60ee9c3658d90b8984c208f
Dállison Lima <79814571+dallisonlima@users.noreply.github.com>
2025-04-15 23:29:57 +0000

    First commit

--- index/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('playButton').addEventListener('click', () => {
-    window.location.href = 'EcoSnake/Game.html';
-  });
-  
-  document.getElementById('aboutButton').addEventListener('click', () => {
-    window.location.href = 'SobreProjeto/SobreProjeto.html';
-  });
-  
